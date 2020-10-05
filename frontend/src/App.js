12149f0e26497558353d8669b14d6d41de64f5a7
RobertoPotato <robertwabuti@gmail.com>
2020-10-05 15:45:47 +0300

    added header, footer, sample products and related components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import Product from "./components/Product"
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to shoppr!</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
