c39e93aa60f67d1ee856f4b18e08d18c843eb636
RobertoPotato <robertwabuti@gmail.com>
2020-11-16 19:54:33 +0300

    route changes and checkout procedure completion

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { addOrderItems } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 export default router;
